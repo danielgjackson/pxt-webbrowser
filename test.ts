422aefdb7e381c0872e9e2464e431d956c7e608a
Daniel Jackson <email@danieljackson.co.uk>
2023-03-15 18:22:17 +0000

    -

--- test.ts
+++ test.ts
@@ -1,1 +1,8 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+
+basic.forever(() => {
+    basic.pause(1000)
+    webbrowser.helloWorld()
+    basic.pause(1000)
+    webbrowser.camlCaseTwo()
+})
