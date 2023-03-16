66d890baaef1683970161d59b016e4c66dbc62e9
Daniel Jackson <email@danieljackson.co.uk>
2023-03-16 16:10:36 +0000

    -

--- main.ts
+++ main.ts
@@ -5,18 +5,66 @@
  * generated for an **exported** function.
  */
 
-//% color="#AA278D" weight=100
+//% color="#AA278D" weight=100 icon="\uf3cd" block="Web Browser Bridge"
 namespace webbrowser {
+
+    let recvHandler: (message: string) => void = function (message: string) {
+        // TODO: Remove default handler
+        basic.showIcon(IconNames.Sad, 500)
+        basic.showString(message)
+    }
+
     //% block
-    export function helloWorld() {
-        basic.showNumber(1)
+    export function startup() {
+        bluetooth.onBluetoothConnected(function () {
+            // TODO: Remove
+            basic.showIcon(IconNames.Yes)
+        })
+
+        bluetooth.onBluetoothDisconnected(function () {
+            // TODO: Remove
+            basic.showIcon(IconNames.No)
+        })
+        
+        bluetooth.onUartDataReceived(bluetooth.NEW_LINE, function () {
+            let recv = bluetooth.uartReadUntil(bluetooth.NEW_LINE)
+            // TODO: Remove
+            //basic.showString(recv)
+            if (recvHandler != null) {
+                recvHandler(recv)
+            }
+        })
+
+        bluetooth.startUartService();
+
+        // TODO: Remove
+        basic.showString("-")
     }
     
-    // note that Caml casing yields lower case
-    // block text with spaces
-    
     //% block
-    export function camlCaseTwo() {
-        basic.showNumber(2)
+    export function send(message: string) {
+        // TODO: Remove
+        //basic.showString(message)
+        bluetooth.uartWriteLine(message)
+// TODO: Remove - only calling on send for debugging
+if (recvHandler != null) {
+recvHandler(message)
+}
     }
+
+    /**
+     * Run code when a certain kind of sprite is created
+     * @param kind
+     * @param sprite
+     */
+    //% draggableParameters="reporter"
+    //% blockId=webbrowseronreceived block="on received $message"
+    //% message.defl=string
+    export function onReceived(handler: (message: string) => void): void {
+// TODO: This is not setting the handler properly?
+        recvHandler = handler
+// TODO: Remove
+basic.showIcon(IconNames.Heart, 500)
+    }
+
 }
--- test.ts
+++ test.ts
@@ -1,8 +1,14 @@
-// tests go here; this will not be compiled when this package is used as an extension.
-
-basic.forever(() => {
+webbrowser.onReceived(function (message) {
+    basic.showIcon(IconNames.Happy, 500)
+    basic.showString(message)
+})
+let count = 0
+webbrowser.startup()
+basic.forever(function () {
     basic.pause(1000)
-    webbrowser.helloWorld()
-    basic.pause(1000)
-    webbrowser.camlCaseTwo()
+    count += 1
+    if (count >= 10) {
+        count = 0
+    }
+    webbrowser.send(convertToText(count))
 })
