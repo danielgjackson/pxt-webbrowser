3bf9d72d4866bdfcefc52be959f1a72f4de4b898
Daniel Jackson <email@danieljackson.co.uk>
2023-03-17 10:14:20 +0000

    -

--- main.ts
+++ main.ts
@@ -1,70 +1,162 @@
 /**
- * Functions are mapped to blocks using various macros
- * in comments starting with %. The most important macro
- * is "block", and it specifies that a block should be
- * generated for an **exported** function.
+ * Web browser bridge
  */
 
-//% color="#AA278D" weight=100 icon="\uf3cd" block="Web Browser Bridge"
-namespace webbrowser {
+//% color="#AA278D" weight=100 icon="\uf3cd" block="Browser Bridge"
+namespace browserBridge {
 
-    let recvHandler: (message: string) => void = function (message: string) {
-        // TODO: Remove default handler
-        basic.showIcon(IconNames.Sad, 500)
-        basic.showString(message)
+    enum Connection {
+        Broadcast,  // both Bluetooth and serial (if enabled)
+        Bluetooth,
+        Serial,
     }
 
-    //% block
-    export function startup() {
+    let started = false
+    let enabledBluetooth = false
+    let connectedBluetooth = false
+    let enabledSerial = false
+
+    let handlerReceivedString: (message: string) => void
+    let handlerReceivedValue: (name: string, value: number) => void
+    let handlerConnected: () => void
+    let handlerDisconnected: () => void
+
+    function startup() {
+        if (started) return
+        // TODO: Any global start-up logic
+
+        started = true
+    }
+
+    //% block="start Bluetooth browser bridge"
+    export function startupBluetooth() {
+        startup()
         bluetooth.onBluetoothConnected(function () {
-            // TODO: Remove
-            basic.showIcon(IconNames.Yes)
+            connectedBluetooth = true
+            if (handlerConnected) {
+                handlerConnected()
+            }
         })
 
         bluetooth.onBluetoothDisconnected(function () {
-            // TODO: Remove
-            basic.showIcon(IconNames.No)
+            connectedBluetooth = false
+            if (handlerDisconnected) {
+                handlerDisconnected()
+            }
         })
         
         bluetooth.onUartDataReceived(bluetooth.NEW_LINE, function () {
-            let recv = bluetooth.uartReadUntil(bluetooth.NEW_LINE)
-            // TODO: Remove
-            //basic.showString(recv)
-            if (recvHandler != null) {
-                recvHandler(recv)
-            }
+            const line = bluetooth.uartReadUntil(bluetooth.NEW_LINE)
+            receivedLine(Connection.Bluetooth, line)
         })
 
         bluetooth.startUartService();
 
-        // TODO: Remove
-        basic.showString("-")
+        enabledBluetooth = true
+    }
+
+    //% block="start serial browser bridge"
+    export function startupSerial() {
+        startup()
+        serial.setRxBufferSize(64)
+        serial.setTxBufferSize(64)
+        serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
+            const line = serial.readUntil(serial.delimiters(Delimiters.NewLine))
+            receivedLine(Connection.Serial, line)
+        })
+        enabledSerial = true
+    }
+
+    // Received packet from a connection
+    function receivedLine(connection: Connection, line: string) {
+        let response = null
+        // Differentiate string (or value) and objects
+        if (line.length <= 0 || (line[0] != '{' && line[0] != '[')) {
+            // Check whether it can be handled as name:value
+            const parts = line.split(':')
+            const name = parts[0]
+            const valueString = parts.slice(1).join(':')
+            const value = parseFloat(valueString)
+            
+            if (!isNaN(valueString as any) && !isNaN(parseFloat(valueString))) {
+                // Value type
+                if (handlerReceivedValue != null) {
+                    handlerReceivedValue(name, value)
+                }
+            } else {
+                // String type
+                if (handlerReceivedString != null) {
+                    handlerReceivedString(line)
+                }
+            }
+        } else {
+            const data = JSON.parse(line)
+            // TODO: Handle object data
+
+        }
+        if (response !== null) {
+            transmit(connection, response)
+        }
+    }
+
+    // Transmit packet to one or more connections
+    function transmit(connection: Connection, data: object) {
+        const line = JSON.stringify(data)
+        if (connectedBluetooth && (connection == Connection.Bluetooth || connection == Connection.Broadcast)) {
+            bluetooth.uartWriteLine(line)
+        }
+        if (enabledSerial && (connection == Connection.Serial || connection == Connection.Broadcast)) {
+            serial.writeLine(line)
+        }
     }
     
     //% block
-    export function send(message: string) {
-        // TODO: Remove
-        //basic.showString(message)
-        bluetooth.uartWriteLine(message)
-// TODO: Remove - only calling on send for debugging
-if (recvHandler != null) {
-recvHandler(message)
-}
+    export function sendString(message: string) {
+        const data = {
+            type: 'string',
+            value: message,
+        }
+        transmit(Connection.Broadcast, data)
     }
 
     /**
-     * Run code when a certain kind of sprite is created
-     * @param kind
-     * @param sprite
+     * Run code when a Bluetooth connection is made
+     */
+    //% block="on Bluetooth browser bridge connection"
+    export function onConnected(handler: () => void): void {
+        handlerConnected = handler
+    }
+
+    /**
+     * Run code when a Bluetooth connection is lost
+     */
+    //% block="on Bluetooth browser bridge disconnection"
+    export function onDisconnected(handler: () => void): void {
+        handlerDisconnected = handler
+    }
+
+    /**
+     * Run code when a string message is received
+     * @param message
      */
     //% draggableParameters="reporter"
-    //% blockId=webbrowseronreceived block="on received $message"
+    //% block="on received string $message"
     //% message.defl=string
-    export function onReceived(handler: (message: string) => void): void {
-// TODO: This is not setting the handler properly?
-        recvHandler = handler
-// TODO: Remove
-basic.showIcon(IconNames.Heart, 500)
+    export function onReceivedString(handler: (message: string) => void): void {
+        handlerReceivedString = handler
+    }
+
+    /**
+     * Run code when a value is received
+     * @param name
+     * @param value
+     */
+    //% draggableParameters="reporter"
+    //% block="on received value $value named $name"
+    //% name.defl=string
+    //% value.defl=number
+    export function onReceivedValue(handler: (name: string, value: number) => void): void {
+        handlerReceivedValue = handler
     }
 
 }
--- test.ts
+++ test.ts
@@ -1,14 +1,20 @@
-webbrowser.onReceived(function (message) {
+browserBridge.onReceivedString(function (message) {
     basic.showIcon(IconNames.Happy, 500)
     basic.showString(message)
 })
+browserBridge.onConnected(function () {
+    basic.showIcon(IconNames.Yes, 500)
+})
+browserBridge.onDisconnected(function () {
+    basic.showIcon(IconNames.No, 500)
+})
 let count = 0
-webbrowser.startup()
+browserBridge.startupBluetooth()
 basic.forever(function () {
     basic.pause(1000)
     count += 1
     if (count >= 10) {
         count = 0
     }
-    webbrowser.send(convertToText(count))
+    browserBridge.sendString(convertToText(count))
 })
