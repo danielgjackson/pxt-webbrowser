41574e1b4de900d36e88f75b78b48cb330e55587
Daniel Jackson <email@danieljackson.co.uk>
2023-04-21 02:05:12 +0100

    -

--- client/service-worker.js
+++ client/service-worker.js
@@ -136,4 +136,4 @@ Alternatively:
   sed -i'' '/\!\!\!$/d' service-worker.js && echo -n +`date '+%Y%m%d%H%M%S'`//\!\!\! >> service-worker.js
 */
 const cacheName = 'v'
-+20230421005515//!!!
++20230421010508//!!!
